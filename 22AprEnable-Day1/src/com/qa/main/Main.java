6fe9cfc72b8d3cf76aa644823aa23422bcd76cd7
reece-elder <reece.elder@qa.com>
2022-05-09 10:44:05 +0100

    hello world

---
+++ 22AprEnable-Day1/src/com/qa/main/Main.java
@@ -0,0 +1,22 @@
+package com.qa.main;
+
+// 'Ctrl + /' it makes this line into a comment 
+// This text isn't read by Java, its just description
+
+// When writing Java it is SUPER IMPORTANT ALL CODE GOES BETWEEN THE {}
+public class Main {
+	
+	// Within Java, the only code that is ever ran is read from the 'main method'
+	// At this stage this is just a 'magic' command that runs code
+	public static void main(String[] args) {
+		// Anything we want our code to do, must go inbetween the {}
+		
+		// System.out.println("some text"); -> print out to the console whatever text we put in
+		System.out.println("Hello 22AprEnable3!");
+		System.out.println("Hello World.");
+		
+	}
+	
+}
+
+// There shouldn't be ANYTHING under the }
