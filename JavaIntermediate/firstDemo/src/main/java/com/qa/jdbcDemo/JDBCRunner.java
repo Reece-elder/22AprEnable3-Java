be5dab6a8b7878a7b39d7ba656c38cc5b8fb7eec
reece-elder <reece.elder@qa.com>
2022-05-18 14:51:04 +0100

    jdbc get requests

--- JavaIntermediate/firstDemo/src/main/java/com/qa/jdbcDemo/JDBCRunner.java
+++ JavaIntermediate/firstDemo/src/main/java/com/qa/jdbcDemo/JDBCRunner.java
@@ -13,8 +13,11 @@ public class JDBCRunner {
 		Drink latte = new Drink("latte", "grande", true, 1.50f);
 		Drink drink2 = new Drink("caramel frappe", "venti", false, 4.15f);
 		
-		newCafe.addDrink(latte);
-		newCafe.addDrink(drink2);
+		System.out.println(newCafe.getDrinkById(10));
+		System.out.println(newCafe.getDrinkById(15));
+		System.out.println("*************************************");
+		System.out.println(newCafe.getAllDrinks());
+		newCafe.getDrinkById(10); // gives us a useless id reference
 	}
 
 }
